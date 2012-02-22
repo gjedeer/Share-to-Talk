d5a04db1b1052e720e80bf256707f5732b62ef07
GDR\! <gdr@go2.pl>
2012-02-22 18:35:37 +0100

    icon

---
+++ generate_icons.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+convert icon.png -resize 36x36 res/drawable-ldpi/ic_launcher.png
+convert icon.png -resize 48x48 res/drawable-mdpi/ic_launcher.png
+convert icon.png -resize 72x72 res/drawable-hdpi/ic_launcher.png
+convert icon.png -resize 96x96 res/drawable-xhdpi/ic_launcher.png
